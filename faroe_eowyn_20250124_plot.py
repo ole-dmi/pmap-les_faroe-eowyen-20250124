775afb690e00c9da3bd97c2482e054e61ce2292a
ole-dmi <oli@dmi.dk>
2025-10-15 12:46:24 +0300

    plotting ..

--- faroe_eowyn_20250124_plot.py
+++ faroe_eowyn_20250124_plot.py
@@ -2,26 +2,37 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import numpy as np
 
-input_dir = "/projappl/project_465000527/olindber/data/pmap-les/faroe_eowyn_20250124/output/500000x500000_251x251"
-
-for i in range(0,90,10):
+input_dir = "/projappl/project_465000527/olindber/data/pmap-les/pmap-les_faroe-eowyen-20250124/output/500000x500000_251x251"
+for i in range(0,94,1):
     filename = f"{input_dir}/data_{i}.nc"
     print(f"Opening ${filename} ...")
     ds       = xr.open_dataset(filename)
+    
+    print(ds)
+    
+    
+    x = np.array(ds['xcr']).transpose()
+    y = np.array(ds['ycr']).transpose()
+    
     u = ds['uvelx'].isel(time=0)[:,:,0]
     v = ds['uvely'].isel(time=0)[:,:,0]
     w = ds['uvelz'].isel(time=0)[:,:,0]
 
     vel = np.sqrt(u**2 + v**2 + w**2)
-
-    plt.figure()
-    plt.pcolormesh(vel)
+    
+    print(x.shape)
+    print(y.shape)
+    print(u.shape)
+    print(v.shape)
+    print(w.shape)
+    
+    plt.pcolormesh(x,y,vel,cmap="coolwarm",vmin=5,vmax=15)
     
     filename = f"{input_dir}/data_{i}.png"
 
     print(f"Saving ${filename} ...")
-#    plt.savefig(filename)
+    plt.savefig(filename)
 #    plt.figure()
 #    plt.pcolormesh(uvelx[:,51,:])
 
-plt.show()
+#plt.show()
